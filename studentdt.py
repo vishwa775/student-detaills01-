ba5fc792dc1f791e63501f7190cff7fa8e6cf3fc
vishwa775 <vishwaamagi@gmail.com>
2025-11-20 14:20:14 +0530

    Add student information script

---
+++ studentdt.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv) != 3:
+  print("Usage: python student.py <name> <roll number>
+        sys.exist(1)
+
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+print("Script Name:",script_name)
+print("Student Name:", name)
+print("Roll Number:", rollno)
