af72d622bc8adb03e412a65c9827c991cdcee840
vishwa775 <vishwaamagi@gmail.com>
2025-11-20 14:40:54 +0530

    Update studentdt.py

--- studentdt.py
+++ studentdt.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) != 3:
-  print("Usage: python student.py <name> <roll number>
+  print("Usage: python student.py <name> <rollno>
         sys.exist(1)
 
 script_name = sys.argv[0]
